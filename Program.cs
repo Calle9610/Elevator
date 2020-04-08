657b8aaa31e25ca332db53ba21f8ae7c7be12cec
Calle9610 <63292150+Calle9610@users.noreply.github.com>
2020-04-08 10:53:26 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,7 +8,7 @@ namespace Elevator
     {
         static void Main(string[] args)
         {
-            
+
             Floor zero = new Floor(0);
             Floor one = new Floor(1);
             Floor two = new Floor(2);
@@ -22,12 +22,28 @@ namespace Elevator
 
 
             List<Floor> Levels = new List<Floor> { zero, one, two, three, four, five, six, seven, eight, nine };
-            Elevator Otis = new Elevator(Levels, 10);
+            Elevator Otis = new Elevator(Levels, 10, 0, 0, 6, null);
 
             Passenger Adam = new Passenger(6);
-            zero.AddPassenger(Adam);
-            Otis.Drive(Levels, 6);
-          
+            Passenger Ture = new Passenger(4);
+            Passenger Bengt = new Passenger(7);
+            Passenger Astrid = new Passenger(1);
+            zero.AddPassengerQueue(Adam);
+            nine.AddPassengerQueue(Ture);
+            zero.AddPassengerQueue(Bengt);
+            seven.AddPassengerQueue(Astrid);
+
+           // Start(Otis);// Nåt har blibit konstigt här...
+            Otis.AddPassengerUpp(zero.Queue);
+            Otis.SetDestination(Otis.CurrentPassenger);
+            Otis.GoUpp(Levels, Otis.ElevatorDirection);
+            Console.ReadLine();
+
+        }
+       static public void Start(Elevator o)
+        {
+            o.AddPassengerUpp(o.CurrentPassenger);
+            o.SetDestination(o.CurrentPassenger);
             
         }
 
